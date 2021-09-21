26853a7a5a0810f2832b35454e8b4bd358502b4c
ShuwenHe <1201220707@pku.edu.cn>
2021-09-21 21:39:38 +0800

    feat: robot

--- hello/hello.c
+++ hello/hello.c
@@ -2,6 +2,9 @@
 
 int main()
 {
-    printf("hello,richad i like my daddy mammy and codings                                  \n");
+    printf("hello,richad i like my daddy mammy and coding\n");
+    printf("i like ai robot");
+    printf("I want to own a rocket");
+    printf("我想拥有一艘火箭");
     return 0;
 }
