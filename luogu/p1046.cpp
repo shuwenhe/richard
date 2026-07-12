6380fdbc5f138b02ed6fc0f4048dd8bd343b1abc
Richard <1201220707@pku.edu.cn>
2026-07-12 16:31:46 +0800

    实现模型训练模块

---
+++ luogu/p1046.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int a[10];
+	int h;
+	cin>>h;
+	h = h + 30;
+	int sum = 0;
+	for(int i = 1;i <= 10;i++){
+		cin>>a[i];
+	}
+	for(int i = 1;i <= 10;i++){
+		if(a[i] >= h){
+			sum++;
+		}
+	}
+	cout<<sum<<endl;
+}
